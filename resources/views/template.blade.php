2ee10aa8ce2733b041d21a5a3489fbb7357ebd55
iwan rhosadi <iwan.rhosadi@gmail.com>
2021-07-26 16:41:42 +0700

    edit footer

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -375,7 +375,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-12">
-                                <span class="footer-text">2019 © stacks</span>
+                                <span class="footer-text">2021 © The Innovation Digital of Geoscience</span>
                             </div>
                         </div>
                     </div>
