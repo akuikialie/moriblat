73b3a8db029cfa8537efffaa290d00e67c89a674
iwan rhosadi <iwan.rhosadi@gmail.com>
2021-07-30 14:34:03 +0700

    add navbar dan footer

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,50 @@
+<div class="horizontal-bar-menu">
+    <ul>
+        <li><a href="index.html" class="active">Dashboard</a></li>
+        <li>
+            <a href="#">Lembaga<i class="material-icons"></i></a>
+        </li>
+        <li>
+            <a href="#">Masyarakat<i class="material-icons"></i></a>
+        </li>
+        <li><a href="#">kajian<i class="material-icons">keyboard_arrow_down</i></a>
+            <ul>
+                <li>
+                    <a href="styles-typography.html">Permasalahan</a>
+                </li>
+                <li>
+                    <a href="styles-code.html">Program</a>
+                </li>
+                <li>
+                    <a href="styles-tables.html">Kegiatan</a>
+                </li>
+            </ul>
+        </li>
+        <li><a href="#">Monev<i class="material-icons">keyboard_arrow_down</i></a>
+            <ul>
+                <li>
+                    <a href="ui-alerts.html">Pemantauan</a>
+                </li>
+                <li>
+                    <a href="ui-badge.html">Evaluasi</a>
+                </li>
+            </ul>
+        </li>
+        <li><a href="#">Kondisi Fisik<i class="material-icons">keyboard_arrow_down</i></a>
+            <ul>
+                <li>
+                    <a href="extended-select2.html">Batimetri</a>
+                </li>
+                <li>
+                    <a href="extended-datatables.html">Foto Udara</a>
+                </li>
+                <li>
+                    <a href="extended-blockui.html">Sedimentasi</a>
+                </li>
+                <li>
+                    <a href="extended-session.html">Data Spasial Lain</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+</div>
